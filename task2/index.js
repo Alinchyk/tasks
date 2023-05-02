749d1184233a7d355dafacc8359a19d3a11918dc
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-05-02 19:55:37 +0200

    update

--- task2/index.js
+++ task2/index.js
@@ -1,12 +1,21 @@
+// 1. Елементы вокруг переменной {{code}} обернуть в тег <span></span>.  Саму же переменную {{code}} обернуть в тег <b></b>
+// 2. Заменить переменные {{variables}} подставив соответствующие значения из переменной couponData
+
 const couponData = {
   code: '20CW-2T5V-RNAN',
   value: 15,
   type: '%',
 };
 
-const str = 'Use coupon code {{code}} get {{value}} {{type}} off for all';
+function fnA(str) {
+  str = str
+    .replace('{{code}}', `<span><b>${couponData.code}</b></span>`)
+    .replace('{{value}}', couponData.value)
+    .replace('{{type}}', couponData.type);
 
-// 1. Елементы вокруг переменной {{code}} обернуть в тег <span></span>.  Саму же переменную {{code}} обернуть в тег <b></b>
-// 2. Заменить переменные {{variables}} подставив соответствующие значения из переменной couponData
+  return `<span>${str}</span>`;
+}
+
+const str = 'Use coupon code {{code}} get {{value}} {{type}} off for all';
 
 console.log(fnA(str)); // "<span>Use coupon code </span><b>20CW-2T5V-RNAN</b><span> get 15 % off for all</span>"
--- task3/index.js
+++ task3/index.js
@@ -1,10 +1,24 @@
 const obj = {
-  "Overall,_how_satisfied_were_you_with_our_service?": "Very_Satisfied",
-  "Would_you_use_our_service_again?": "Probably_Not",
-  "Would_you_recommend_us_to_a_friend?": "Probably",
+  'Overall,_how_satisfied_were_you_with_our_service?': 'Very_Satisfied',
+  'Would_you_use_our_service_again?': 'Probably_Not',
+  'Would_you_recommend_us_to_a_friend?': 'Probably',
 };
 
-console.log(fnA(obj)); // [
+const fnA = function (obj) {
+  const entries = Object.entries(obj);
+
+  // return entries.map(([title, value]) => {
+  //   return { title: title.replace(/_/g, ' '), value: value.replace(/_/g, ' ') };
+  // });
+
+  return entries.reduce((acc, [title, value]) => {
+    acc.push({ title: title.replace(/_/g, ' '), value: value.replace(/_/g, ' ') });
+    return acc;
+  }, []);
+};
+
+console.log(fnA(obj));
+// [
 //    { title: "Overall, how satisfied were you with our service?, value: "Very Satisfied"},
 //    { title: "Would you use our service again?", value: "Probably Not"},
 //    { title: "Would you recommend us to a friend?", value: "Probably"}
